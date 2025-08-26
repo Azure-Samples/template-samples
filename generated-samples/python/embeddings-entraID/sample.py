8b6cc58cf2ec063c759013035dd3164341c4aa45
kaylieee <kaylieleung1@gmail.com>
2025-08-26 18:22:31 +0000

    add python embeddings templates and samples

---
+++ generated-samples/python/embeddings-entraID/sample.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+endpoint = "<%= endpoint %>"
+deployment_name = "<some-deployment-name>"
+token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+
+client = OpenAI(
+    base_url = endpoint,
+    azure_ad_token_provider = token_provider,
+)
+
+response = client.embeddings.create(
+    input = "How do I use Python in VS Code?",
+    model = deployment_name
+)
+print(response.data[0].embedding)
